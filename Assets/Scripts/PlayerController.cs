57e6d9576c2e190beb8f18ebe2ec6f1037b364e8
Lee JaeHyun <myleje@gmail.com>
2026-03-19 21:24:09 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamageable
 {
     [SerializeField] private PlayerDataSO playerData;
     [SerializeField] private float currHp;
+
+    [SerializeField] private HealthEventSO healthEventSO;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currHp = playerData.maxHp;   
     }
+
+    public void TakeDamage(float damage)
+    {
+        currHp -= damage;
+        Debug.Log($"피격 데미지 {damage} , 현재 HP {currHp}");
+        
+        // 데미지 처리를 위한 이벤트 발생 요청
+        healthEventSO.Raise(currHp);
+    }
+
+    public void Update()
+    {
+        // if (Input.GetMouseButtonDown(0))  //=> Legacy Input Manager
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            TakeDamage(10.0f);
+        }
+    }
     
+}
+
+public interface IDamageable
+{
+    void TakeDamage(float damage);
 }
