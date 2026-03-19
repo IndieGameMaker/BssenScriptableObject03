fa9d4699a84ded2979eb072dba42314a43ccce1c
Lee JaeHyun <myleje@gmail.com>
2026-03-19 21:17:12 +0900

    .

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -1,8 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
     [SerializeField] private Image hpBar;
-    [SerializeField] private HealthEventSO HealthEventSO;
+    [SerializeField] private HealthEventSO healthEventSO;
+    
+    // 이벤트 수신
+    private void OnEnable()
+    {
+        healthEventSO.Subscribe(OnHpChanged);
+    }
+
+    // 이벤트 해지
+    private void OnDisable()
+    {
+        healthEventSO.Unsubscribe(OnHpChanged);
+    }
+
+    // 이벤트 발생시 호출할 메서드
+    private void OnHpChanged(float hp)
+    {
+        hpBar.fillAmount = hp / 100.0f;
+    }
 }
